c518f6afc2dff8c8c82fdfd578947b4d5d667e51
Chase Wex <clapingmanly@hotmail.com>
2019-06-14 12:30:58 -0400

    added basic code for endpoints

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const db = require('../database/db_model');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,17 +11,26 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let {username, password} = req.body;
+  if(!username || username === "" || !password || password === "") return res.status(400).json({error: "invalid data in body", message: "must enter a username and password"})
+  db.register(username, password)
+  .then(result => res.status(201).json(result))
+  .catch(err => res.status(400).json({error: "invalid request", message: err}));
 }
 
 function login(req, res) {
   // implement user login
+  let {username, password} = req.body;
+  if(!username || username === "" || !password || password === "") return res.status(400).json({error: "invalid data in body", message: "must enter a username and password"})
+  db.login(username,password)
+  .then(result => res.status(201).json(result))
+  .catch(err => res.status(400).json({error: "invalid request", message: err}));
 }
 
 function getJokes(req, res) {
   const requestOptions = {
     headers: { accept: 'application/json' },
   };
-
   axios
     .get('https://icanhazdadjoke.com/search', requestOptions)
     .then(response => {
